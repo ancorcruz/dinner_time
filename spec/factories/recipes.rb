98c1ce18bdbac6e8fa9e8d7d46c489d1e0ed7b8b
Ancor Cruz <ancor.cruz.ext@qonto.com>
2023-07-06 20:21:32 +0100

    Add Recipe model

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :recipe do
+    title { "Golden Sweet Cornbread" }
+    cook_time { 25 }
+    prep_time { 10 }
+    ratings { 4 }
+    cuisine { "Nuveau Cousine" }
+    category { "Cornbread" }
+    author { "bluegirl" }
+    image { "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2021%2F10%2F26%2Fcornbread-1.jpg" }
+  end
+end
